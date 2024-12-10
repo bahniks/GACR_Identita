ab8e2c8e0b1f03d2ec0fcd9ef34576c300d94911
Stepan Bahnik <bahniks@seznam.cz>
2024-12-10 15:37:48 +0100

    Update trustgame.py

--- Stuff/trustgame.py
+++ Stuff/trustgame.py
@@ -35,17 +35,18 @@ Na konci studie se dozvíte, jaká byla Vaše role a jaký je celkový výsledek
 
 
 instructionsT2 = """<b>Pro účastníka studie, s kterým jste spárován(a), jsou blízké tyto skupiny:
-{}
+{}</b>
 
-On(a) podobně bude vědět, jaké skupiny jsou blízké Vám.</b>
+On(a) podobně bude vědět, jaké skupiny jsou blízké Vám.
 
 <i>Hráč A:</i> Má možnost poslat hráči B od 0 do {} Kč (po {} Kč). Poslaná částka se ztrojnásobí a obdrží ji hráč B.
 <i>Hráč B:</i> Může poslat zpět hráči A jakékoli množství peněz získaných v této úloze, tedy úvodních {} Kč a ztrojnásobenou částku poslanou hráčem A.
 
 Předem nebudete vědět, jaká je Vaše role a uvedete tedy rozhodnutí pro obě role.
-Kromě Vašich rozhodnutí uveďte také, kolik očekáváte, že Vám pošle zpět hráč B, pokud bude náhodně vybráno, že jste hráč A.
 
-Svou volbu učiňte posunutím modrých ukazatelů níže."""
+Svou volbu učiňte posunutím modrých ukazatelů níže.
+
+Až se rozhodnete u všech možností, uveďte pomocí ukazatele, kolik očekáváte, že Vám pošle zpět hráč B, pokud bude náhodně vybráno, že jste hráč A."""
 
 
 
@@ -68,9 +69,6 @@ trustControl3 = "Jakou odměnu obdrží hráč B, pokud hráč A pošle 40 Kč a
 trustAnswers3 = ["80 Kč (100 + 40 - 60)", "160 Kč (100 + 3 × 40 - 60)", "240 Kč (100 + 3 × 60 - 40)", "280 Kč (100 + 3 × 40 + 60)"]
 trustFeedback3 = ["Chybná odpověď. Hráč B obdrží 100 Kč, ke kterým obdrží 120 Kč od hráče A (poslaných 40 Kč se ztrojnásobí) a následně pošle hráči A 60 Kč, tj. na konec obdrží 160 Kč (100 + 3 × 40 - 60).", "Správná odpověď.", "Chybná odpověď. Hráč B obdrží 100 Kč, ke kterým obdrží 120 Kč od hráče A (poslaných 40 Kč se ztrojnásobí) a následně pošle hráči A 60 Kč, tj. na konec obdrží 160 Kč (100 + 3 × 40 - 60).", "Chybná odpověď. Hráč B obdrží 100 Kč, ke kterým obdrží 120 Kč od hráče A (poslaných 40 Kč se ztrojnásobí) a následně pošle hráči A 60 Kč, tj. na konec obdrží 160 Kč (100 + 3 × 40 - 60)."]
 
-
-
-wait_text = "Prosím počkejte na druhého hráče."
 
 
 
@@ -97,9 +95,6 @@ checkButtonText = "Rozhodl(a) jsem se u všech možností"
 
 
 ################################################################################
-
-
-
 
 
 class ScaleFrame(Canvas):
@@ -131,21 +126,26 @@ class ScaleFrame(Canvas):
 
         self.valueLab = ttk.Label(self, textvariable = self.valueVar, font = "helvetica {}".format(font), background = "white", width = 3, anchor = "e")
         self.currencyLab = ttk.Label(self, text = "Kč", font = "helvetica {}".format(font), background = "white", width = 6)
-        self.playerLab1 = ttk.Label(self, text = self.playerText1, font = "helvetica {}".format(font), background = "white", width = 6, anchor = "e") 
-        self.playerLab2 = ttk.Label(self, text = self.playerText2, font = "helvetica {}".format(font), background = "white", width = 6, anchor = "e") 
-        self.totalLab1 = ttk.Label(self, text = self.totalText1.format(0), font = "helvetica {}".format(font), background = "white", width = 6, anchor = "e")
-        self.totalLab2 = ttk.Label(self, text = self.totalText2.format(0), font = "helvetica {}".format(font), background = "white", width = 6, anchor = "e")
-        self.spaces = ttk.Label(self, text = " ", font = "helvetica {}".format(font), background = "white", width = 1)
-        self.changedValue(0)
 
         self.value.grid(column = 1, row = 1, padx = 10)
         self.valueLab.grid(column = 3, row = 1)        
         self.currencyLab.grid(column = 4, row = 1)
+
+        fg = "white" if not self.player else "black"
+
+        self.playerLab1 = ttk.Label(self, text = self.playerText1, font = "helvetica {}".format(font), background = "white", width = 6, anchor = "e", foreground = fg) 
+        self.playerLab2 = ttk.Label(self, text = self.playerText2, font = "helvetica {}".format(font), background = "white", width = 6, anchor = "e", foreground = fg) 
+        self.totalLab1 = ttk.Label(self, text = self.totalText1.format(0), font = "helvetica {}".format(font), background = "white", width = 6, anchor = "e", foreground = fg)
+        self.totalLab2 = ttk.Label(self, text = self.totalText2.format(0), font = "helvetica {}".format(font), background = "white", width = 6, anchor = "e", foreground = fg)
+        self.spaces = ttk.Label(self, text = " ", font = "helvetica {}".format(font), background = "white", width = 1)
+
         self.playerLab1.grid(column = 5, row = 1, padx = 3)
         self.totalLab1.grid(column = 6, row = 1, padx = 3, sticky = "ew")
-        self.spaces.grid(column = 7, row = 1)
         self.playerLab2.grid(column = 8, row = 1, padx = 3)        
         self.totalLab2.grid(column = 9, row = 1, padx = 3, sticky = "ew")
+        self.spaces.grid(column = 7, row = 1)        
+        
+        self.changedValue(0)
 
 
     def onClick(self, event):
@@ -165,7 +165,7 @@ class ScaleFrame(Canvas):
             self.totalLab2["text"] = self.totalText2.format(self.endowment + newval * 3)
             self.totalLab1["font"] = "helvetica {} bold".format(self.font)
             self.playerLab1["font"] = "helvetica {} bold".format(self.font)
-        else:
+        elif self.player == "B":
             self.totalLab1["text"] = self.totalText1.format(self.endowment - self.returned + newval)
             self.totalLab2["text"] = self.totalText2.format(self.returned * 3 + self.endowment - newval)
             self.totalLab2["font"] = "helvetica {} bold".format(self.font)
@@ -189,7 +189,7 @@ class Trust(InstructionsFrame):
         otherInfo = "\n".join(otherInfo[:4])
         text = instructionsT2.format(otherInfo, endowment, endowment, int(endowment/5), endowment)
 
-        height = 26
+        height = 20
         width = 102
 
         super().__init__(root, text = text, height = height, font = 15, width = width)
@@ -202,31 +202,36 @@ class Trust(InstructionsFrame):
         self.labR.grid(column = 1, row = 2, pady = 5, sticky = E)
 
         self.labX = ttk.Label(self, text = "Finální rozdělení odměn", font = "helvetica 15 bold", background = "white", anchor = "center", width = 28)
-        self.labX.grid(column = 1, row = 5, pady = 5, sticky = E)
+        self.labX.grid(column = 1, row = 6, pady = 5, sticky = E)
 
         self.frames = {}
-        for i in range(7):            
-            if i != 6:
+        for i in range(8):            
+            if i < 6:
                 text = "Pokud hráč A pošle {} Kč, pošlu hráči A zpět:".format(int(i*endowment/5))
-                ttk.Label(self, text = text, font = "helvetica 15", background = "white").grid(column = 0, row = 6 + i, pady = 1, sticky = E)
+                ttk.Label(self, text = text, font = "helvetica 15", background = "white").grid(column = 0, row = 7 + i, pady = 1, sticky = E)
                 player = "B"
-            else:
+            elif i == 6:
                 ttk.Label(self, text = "Pošlu hráči B:", font = "helvetica 15", background = "white").grid(column = 0, row = 3, pady = 1, sticky = E)            
                 player = "A"
+            else:
+                ttk.Label(self, text = "Očekávám, že hráč B pošle zpět:", font = "helvetica 15", background = "white").grid(column = 0, row = 4, pady = 1, sticky = E)  
+                player = None
             maximum = int(i * 3 * endowment / 5 + endowment) if i < 6 else endowment            
             self.frames[i] = ScaleFrame(self, maximum = maximum, player = player, returned = int(i*endowment/5), endowment = endowment)
-            row = 6 + i if i < 6 else 3
+            row = 7 + i if i < 6 else i - 3
             self.frames[i].grid(column = 1, row = row, pady = 1)
+            if i == 7:
+                self.frames[i].value["state"] = "disabled"
         
         self.labB = ttk.Label(self, text = "Pokud budu hráč B", font = "helvetica 15 bold", background = "white")
-        self.labB.grid(column = 0, row = 5, columnspan = 3, pady = 10)
+        self.labB.grid(column = 0, row = 6, columnspan = 3, pady = 10)
 
         self.checkVar = BooleanVar()
         ttk.Style().configure("TCheckbutton", background = "white", font = "helvetica 15")
         self.checkBut = ttk.Checkbutton(self, text = checkButtonText, command = self.checkbuttoned, variable = self.checkVar, onvalue = True, offvalue = False)
-        self.checkBut.grid(row = 18, column = 0, columnspan = 3)
+        self.checkBut.grid(row = 19, column = 0, columnspan = 3, pady = 10)
 
-        self.next.grid(column = 0, row = 19, columnspan = 3, pady = 5, sticky = N)            
+        self.next.grid(column = 0, row = 20, columnspan = 3, pady = 5, sticky = N)            
         self.next["state"] = "disabled"
         
         self.text.grid(row = 1, column = 0, columnspan = 3)
@@ -246,6 +251,15 @@ class Trust(InstructionsFrame):
 
     def checkbuttoned(self):
         self.next["state"] = "normal" if self.checkVar.get() else "disabled"
+        for i, frame in self.frames.items():
+            if i != 7:
+                frame.value["state"] = "normal" if not self.checkVar.get() else "disabled"
+            else:
+                frame.value["state"] = "normal" if self.checkVar.get() else "disabled"
+                frame.maximum = TRUST + int(self.frames[6].valueVar.get()) * 3
+                frame.value["to"] = frame.maximum
+
+        
 
     def nextFun(self):
         self.send()
@@ -274,7 +288,7 @@ class Trust(InstructionsFrame):
 
 class WaitResults(InstructionsFrame):
     def __init__(self, root):
-        super().__init__(root, text = wait_text, height = 3, font = 15, proceed = False, width = 45)
+        super().__init__(root, text = "", height = 3, font = 15, proceed = False, width = 45)
         self.progressBar = ttk.Progressbar(self, orient = HORIZONTAL, length = 400, mode = 'indeterminate')
         self.progressBar.grid(row = 2, column = 1, sticky = N)
 
